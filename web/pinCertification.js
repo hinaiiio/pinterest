f4e1be3c1dfad55750468b904244d7aab0e5a729
Hina <bob.piyo.1995@gmail.com>
2016-12-23 17:45:26 +0900

    addSDK

--- web/pinCertification.js
+++ web/pinCertification.js
@@ -1,17 +1,11 @@
 /**
  * Created by hina on 2016/12/06.
  */
-window.pAsyncInit = function() {
-    PDK.init({
-        appId: "<your app-id>", // Change this
-        cookie: true
-    });
+var a = "hello,world";
+
+
+var params = {
+    fields: 'id,note,link,image'
 };
 
-(function(d, s, id){
-    var js, pjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//assets.pinterest.com/sdk/sdk.js";
-    pjs.parentNode.insertBefore(js, pjs);
-}(document, 'script', 'pinterest-jssdk'));
+
